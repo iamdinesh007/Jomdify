d1f67102d3f277eeba46365b30429c9754ae4d5e
iamdinesh007 <82375335+iamdinesh007@users.noreply.github.com>
2023-07-05 12:52:38 +0530

    Update script.js

--- Project_spotify/script.js
+++ Project_spotify/script.js
@@ -10,14 +10,14 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "People", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Softcore", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
     {songName: " Invincible [NCS Release]", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
     {songName: "Janji-Heroes-Tonight", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
     {songName: "Rabba - Dildaara", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
     {songName: "Sakhiyaan - Dildaara", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Dildaara", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Warriyo - Mortals", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
     {songName: "Tumhari Kasam - Dildaara", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
     {songName: "Na Ja Chod ke - Dildaara", filePath: "songs/10.mp3", coverPath: "covers/10.jpg"},
 ]
